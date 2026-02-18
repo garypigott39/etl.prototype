ad2c1e0a3627b906aa5e9610d11c35a7f12af986
Gary Pigott <gary.pigott@capitaleconomics.com>
2026-02-18 12:44:07 +0000

    Schema wip

--- schema/ce_etl/procedures/px_ut_lock_pipeline.sql
+++ schema/ce_etl/procedures/px_ut_lock_pipeline.sql
@@ -25,16 +25,16 @@ BEGIN
     IF _name IS NULL THEN
         RAISE EXCEPTION 'Pipeline name must be provided';
     ELSEIF _type = 'lock' THEN
-        IF (SELECT COUNT(*) FROM ce_etl.s_pipeline_locks WHERE name = _name AND locked_utc > _ts) > 0 THEN
+        IF (SELECT COUNT(*) FROM ce_etl.s_pipeline_lock WHERE name = _name AND locked_utc > _ts) > 0 THEN
             RAISE EXCEPTION 'Pipeline "%" is already locked', _name;
         ELSE
-            DELETE FROM ce_etl.s_pipeline_locks WHERE name = _name;
-            INSERT INTO ce_etl.s_pipeline_locks(name) VALUES (_name);
+            DELETE FROM ce_etl.s_pipeline_lock WHERE name = _name;
+            INSERT INTO ce_etl.s_pipeline_lock(name) VALUES (_name);
             RAISE INFO 'Pipeline "%" locked', _name;
         END IF;
     ELSEIF _type = 'unlock' THEN
         -- Unlock the pipeline
-        DELETE FROM ce_etl.s_pipeline_locks WHERE name = _name;
+        DELETE FROM ce_etl.s_pipeline_lock WHERE name = _name;
         RAISE INFO 'Pipeline "%" unlocked', _name;
     ELSE
         RAISE EXCEPTION 'Invalid type "%" - must be lock or unlock', _type;
--- schema/ce_etl/tables/s_pipeline_lock.sql
+++ schema/ce_etl/tables/s_pipeline_lock.sql
@@ -1,20 +1,20 @@
 /*
  ***********************************************************************************************************
  * @file
- * s_pipeline_locks.sql
+ * s_pipeline_lock.sql
  *
  * System table - generic pipeline "locks", enabling us to lock/unlock a pipeline for processing.
  ***********************************************************************************************************
  */
 
--- DROP TABLE IF EXISTS ce_etl.s_pipeline_locks;
+-- DROP TABLE IF EXISTS ce_etl.s_pipeline_lock;
 
-CREATE TABLE IF NOT EXISTS ce_etl.s_pipeline_locks
+CREATE TABLE IF NOT EXISTS ce_etl.s_pipeline_lock
 (
     name TEXT NOT NULL,
     locked_utc TIMESTAMPTZ NOT NULL DEFAULT NOW(),  -- Locked timestamp
     PRIMARY KEY (name)
 );
 
-COMMENT ON TABLE ce_etl.s_pipeline_locks
+COMMENT ON TABLE ce_etl.s_pipeline_lock
     IS 'System table - generic pipeline "locks", enabling us to lock/unlock a pipeline for processing';

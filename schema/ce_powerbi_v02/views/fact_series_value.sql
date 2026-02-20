3c6411d562b346e854c96536ab706f75e059a313
Gary Pigott <gary.pigott@capitaleconomics.com>
2026-02-20 08:28:46 +0000

    Schema wip

---
+++ schema/ce_powerbi_v02/views/fact_series_value.sql
@@ -0,0 +1,32 @@
+/*
+ ***********************************************************************************************************
+ * @file
+ * fact_series_value.sql
+ *
+ * View - "fact" table for series value.
+ ***********************************************************************************************************
+ */
+
+-- DROP VIEW IF EXISTS ce_powerbi_v02.fact_series_value;
+
+CREATE OR REPLACE VIEW ce_powerbi_v02.fact_series_value
+AS
+    -- As per base materialized view, by specifying cols this allows for easier mods
+    SELECT
+        pk_sv,
+        sv_value,
+        sv_old_value,
+        fk_pk_tip,
+        fk_pk_s,
+        fk_pk_p,
+        fk_pk_d,
+        fk_pk_src,
+        fk_pk_t,
+        fk_pk_f,
+        fk_pk_geo,
+        fk_pk_com,
+        fk_pk_i
+    FROM ce_powerbi.mv_fact_series_value;
+
+COMMENT ON VIEW ce_powerbi_v02.fact_series_value
+    IS 'View - "fact" table for series value';

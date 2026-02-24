6efdbe32af6bf0e84319f012250e6ad7cf3b48f5
Gary Pigott <gary.pigott@capitaleconomics.com>
2026-02-24 22:49:41 +0000

    Schema wip

---
+++ schema/ce_warehouse/functions/fx_tg_audit.sql
@@ -0,0 +1,54 @@
+/*
+ ***********************************************************************************************************
+ * @file
+ * fx_tg_audit.sql
+ *
+ * Trigger function - log changes in generic audit.
+ ***********************************************************************************************************
+ */
+
+-- DROP FUNCTION IF EXISTS ce_warehouse.fx_tg_audit;
+
+CREATE OR REPLACE FUNCTION ce_warehouse.fx_tg_audit(
+)
+    RETURNS TRIGGER
+    LANGUAGE plpgsql
+AS
+$$
+DECLARE
+    _diff JSONB;
+    _pk_col TEXT;
+    _pk INT;
+
+BEGIN
+    -- Check that a PK column was passed
+    IF TG_NARGS < 1 THEN
+        RAISE EXCEPTION 'Trigger requires the primary key column name as argument';
+    END IF;
+    _pk_col := TG_ARGV[0];  -- first argument passed to the trigger
+
+    IF TG_OP = 'INSERT' THEN
+        _pk := (TO_JSONB(NEW) ->> _pk_col)::TEXT;
+        _diff := TO_JSONB(NEW);
+    ELSEIF TG_OP = 'UPDATE' THEN
+        _pk := (TO_JSONB(NEW) ->> _pk_col)::TEXT;
+        _diff := (
+            SELECT JSONB_OBJECT_AGG(key, value)
+            FROM (
+                SELECT key, value
+                FROM JSONB_EACH(TO_JSONB(NEW)) AS new(key, value)
+                WHERE value IS DISTINCT FROM (TO_JSONB(OLD)->key)
+            ) AS changes
+        );
+    ELSEIF TG_OP = 'DELETE' THEN
+        _pk := (TO_JSONB(OLD) ->> _pk_col)::TEXT;
+        _diff := TO_JSONB(OLD);
+    END IF;
+
+    INSERT INTO ce_warehouse.a_audit (table_name, table_pk, data, audit_type)
+        VALUES (TG_TABLE_NAME, _pk::TEXT, _diff, TG_OP);
+END
+$$;
+
+COMMENT ON FUNCTION ce_warehouse.fx_tg_audit
+    IS 'Trigger function - log changes in generic audit';

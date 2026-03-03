3bb5f180340a0d05a4e580319c9f2f4b2cb19407
Gary Pigott <gary.pigott@capitaleconomics.com>
2026-03-03 13:08:20 +0000

    WIP

--- schema/ce_warehouse/procedures/px_ut__lock.sql
+++ schema/ce_warehouse/procedures/px_ut__lock.sql
@@ -1,17 +1,17 @@
 /*
  ***********************************************************************************************************
  * @file
- * px_ut__lock_or_unlock.sql
+ * px_ut__lock.sql
  *
  * Utility procedure - lock/unlock named pipeline (or whatever).
  ***********************************************************************************************************
  */
 
--- DROP PROCEDURE IF EXISTS ce_warehouse.px_ut__lock_or_unlock;
+-- DROP PROCEDURE IF EXISTS ce_warehouse.px_ut__lock;
 
-CREATE OR REPLACE PROCEDURE ce_warehouse.px_ut__lock_or_unlock(
+CREATE OR REPLACE PROCEDURE ce_warehouse.px_ut__lock(
     _name TEXT,
-    _type TEXT DEFAULT 'lock',          -- lock | unlock
+    _type TEXT DEFAULT 'add',
     _interval INTERVAL DEFAULT '1 hour'
 )
     LANGUAGE plpgsql
@@ -25,7 +25,7 @@ BEGIN
         RAISE EXCEPTION 'Lock name must be provided';
     END IF;
 
-    IF _type = 'lock' THEN
+    IF _type = 'add' THEN
         -- Fail if active lock exists
         IF EXISTS (
             SELECT 1 FROM ce_warehouse.s__lock
@@ -44,17 +44,17 @@ BEGIN
                 DO UPDATE
                     SET ts_locked_at = NOW();
 
-    ELSIF _type = 'unlock' THEN
+    ELSIF _type = 'remove' THEN
         DELETE FROM ce_warehouse.s__lock
         WHERE name = _name;
 
     ELSE
-        RAISE EXCEPTION 'Invalid type "%" - must be lock or unlock', _type;
+        RAISE EXCEPTION 'Invalid type "%" - must be "add" or "remove"', _type;
     END IF;
 
-    RAISE INFO 'Lock "%" %ed successfully', _name, _type;
+    RAISE INFO 'Lock "%" % successfully', _name, _type;
 END;
 $$;
 
-COMMENT ON PROCEDURE ce_warehouse.px_ut__lock_or_unlock
+COMMENT ON PROCEDURE ce_warehouse.px_ut__lock
     IS 'Utility procedure - lock/unlock named pipeline (or whatever)';

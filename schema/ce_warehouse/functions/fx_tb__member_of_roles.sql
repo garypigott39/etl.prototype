45b52de04ab0142bf9b37369684dc2ca83059688
Gary Pigott <gary.pigott@capitaleconomics.com>
2026-03-06 14:46:25 +0000

    wip

--- schema/ce_warehouse/functions/fx_tb__member_of_roles.sql
+++ schema/ce_warehouse/functions/fx_tb__member_of_roles.sql
@@ -1,31 +1,31 @@
 /*
  ***********************************************************************************************************
  * @file
- * fx_tb__member_roles.sql
+ * fx_tb__member_of_roles.sql
  *
  * Pseudo table function - provide a list of users (role members) & their roles.
  ***********************************************************************************************************
  */
 
--- DROP FUNCTION IF EXISTS ce_warehouse.fx_tb__member_roles;
+-- DROP FUNCTION IF EXISTS ce_core.fx_tb__member_of_roles;
 
-CREATE OR REPLACE FUNCTION ce_warehouse.fx_tb__member_roles(
+CREATE OR REPLACE FUNCTION ce_core.fx_tb__member_of_roles(
 )
     RETURNS TABLE (
-        member      NAME,
-        role        NAME
+        user_name   NAME,
+        member_of   NAME
     )
     LANGUAGE sql
 AS
 $$
     SELECT
-        r.rolname AS member,
-        g.rolname AS role
+        r.rolname   AS user_name,
+        g.rolname   AS member_of
     FROM pg_auth_members m
-    JOIN pg_roles r ON m.member = r.oid
-    JOIN pg_roles g ON m.roleid = g.oid
+        JOIN pg_roles r ON m.member = r.oid
+        JOIN pg_roles g ON m.roleid = g.oid
     ORDER BY 1, 2;
 $$;
 
-COMMENT ON FUNCTION ce_warehouse.fx_tb__member_roles
+COMMENT ON FUNCTION ce_core.fx_tb__member_of_roles
     IS 'Pseudo table function - provide a list of users (role members) & their roles';
